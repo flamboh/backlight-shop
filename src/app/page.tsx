ad35bf97ff1570fc9ce4ed1ac424319482a4cb75
flamboh <oliver.boorstein@gmail.com>
2025-09-02 23:38:25 -0800

    add shadcn, update background to be animated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-linear-to-tr from-[#904B50] via-[#6F456D] to-[#922054]">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-linear-to-tr from-[#904B50] via-[#724870] to-[#922054] gradient-background">
       <h1 className="text-4xl font-bold text-[#825EA4]">{"What's up?"}</h1>
     </div>
   );
