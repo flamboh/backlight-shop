d67e6c8a31d470dee90ac2ef4f773246b63815af
flamboh <oliver.boorstein@gmail.com>
2025-09-02 23:48:05 -0800

    change to use custom styling for background, increase middle portion prevalence

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-linear-to-tr from-[#904B50] via-[#724870] to-[#922054] gradient-background">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 bg-gradient-to-tr from-pfp-left via-pfp-middle via-40% to-pfp-right gradient-background">
       <h1 className="text-4xl font-bold ">{"What's up?"}</h1>
     </div>
   );
