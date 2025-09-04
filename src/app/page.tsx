6d7ae190498d97afe9dcba195a3e2be1ec97cec1
flamboh <oliver.boorstein@gmail.com>
2025-09-03 21:28:42 -0800

    backlight hero with logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import Navbar from "@/components/ui/navbar";
+import Image from "next/image";
 import * as React from "react";
 
 export default function Home() {
   return (
-    <div className="font-sans flex flex-col items-center justify-items-center min-h-[200vh] animated-gradient">
-      <Navbar />
-
-      <h1 className="text-4xl font-bold ">{"Hello!"}</h1>
-      <Card>
-        <Button>Click me</Button>
-      </Card>
+    <div className="flex flex-col">
+      <div className="font-sans flex flex-col items-center justify-items-center min-h-[100vh] animated-gradient">
+        <Navbar />
+        <div className="flex flex-row items-center justify-items-center pt-[30vh]">
+          <Image
+            src="/favicon.ico"
+            alt="Backlight"
+            className="w-[10vw] h-[10vw]"
+            width={96}
+            height={96}
+          />
+          <h1 className="font-bold font-caveat text-[8vw] text-">
+            {"Backlight"}
+          </h1>
+        </div>
+      </div>
+      <div className="font-sans flex flex-col items-center justify-items-center min-h-[100vh] bg-background border-t border-b border-foreground/40">
+        <h1 className="text-4xl font-bold ">{"Hello!"}</h1>
+        <Card>
+          <Button>Click me</Button>
+        </Card>
+      </div>
     </div>
   );
 }
