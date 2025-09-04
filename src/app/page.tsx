7d10fc9a2e5ccab4d9bea819261433fac3fd679f
flamboh <oliver.boorstein@gmail.com>
2025-09-03 21:12:33 -0800

    updated theming, added dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import Navbar from "@/components/comp-587";
+import Navbar from "@/components/ui/navbar";
+import * as React from "react";
 
 export default function Home() {
   return (
     <div className="font-sans flex flex-col items-center justify-items-center min-h-[200vh] animated-gradient">
       <Navbar />
-      <h1 className="text-4xl font-bold ">{"Hello?"}</h1>
+
+      <h1 className="text-4xl font-bold ">{"Hello!"}</h1>
       <Card>
         <Button>Click me</Button>
       </Card>
