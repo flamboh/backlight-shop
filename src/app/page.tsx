4f600cfd3c622a26aeceb0d929bc1d74e2ef7aed
flamboh <oliver.boorstein@gmail.com>
2025-09-03 21:51:36 -0800

    fuzzy hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,29 @@ import { Card } from "@/components/ui/card";
 import Navbar from "@/components/ui/navbar";
 import Image from "next/image";
 import * as React from "react";
+import FuzzyText from "@/components/ui/text/FuzzyText";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <div className="font-sans flex flex-col items-center justify-items-center min-h-[100vh] animated-gradient">
         <Navbar />
-        <div className="flex flex-row items-center justify-items-center pt-[30vh]">
+        <div className="flex flex-row items-center justify-items-center pt-[30vh] select-none">
           <Image
             src="/favicon.ico"
             alt="Backlight"
-            className="w-[10vw] h-[10vw]"
+            className="w-[clamp(6rem,10vw,16rem)] h-[clamp(6rem,10vw,16rem)]"
             width={96}
             height={96}
           />
-          <h1 className="font-bold font-caveat text-[8vw] text-">
-            {"Backlight"}
+          <h1 className="font-caveat text-bl-light-purple -ml-20">
+            <FuzzyText
+              baseIntensity={0.01}
+              enableHover={false}
+              fontSize={"clamp(4rem,10vw,8rem)"}
+            >
+              {"Backlight"}
+            </FuzzyText>
           </h1>
         </div>
       </div>
